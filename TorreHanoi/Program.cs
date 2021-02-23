e83821b4dda3b5110cc82293c15315a940f331bb
Paulo Nogueira do Nascimento <paulonog061@gmail.com>
2021-02-23 03:16:59 -0300

    Add files via upload

---
+++ TorreHanoi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TorreHanoi
+{
+    class Program
+    {
+        private static void Mover(int origem, int destino)
+        {
+            Console.WriteLine("Move: origem "+ origem +" para: "+ destino);
+        }
+
+        public static void Hanoi(int disco, int origem, int destino, int trabalho)
+        {
+            if(disco > 0)
+            {
+                Hanoi(disco - 1, origem, trabalho,destino);
+                Mover(origem, destino);
+                Hanoi(disco - 1, trabalho, destino, origem);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Hanoi(3, 1, 3, 2);
+            //3 discos, 1 = origem, 3 = destino, 2 = trabalho
+        }
+    }
+}
